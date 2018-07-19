1f8e8cdf1587eb7798e90b192064173a031bfc0d
roba <varga.robert2@gmail.com>
2018-07-19 10:53:23 +0200

    Adding DevBootstrap

---
+++ src/main/java/guru/springframework/sprint5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,47 @@
+package guru.springframework.sprint5webapp.bootstrap;
+
+import guru.springframework.sprint5webapp.model.Author;
+import guru.springframework.sprint5webapp.model.Book;
+import guru.springframework.sprint5webapp.repositories.AuthorRepository;
+import guru.springframework.sprint5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    private void initData(){
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design","1234","Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        //Rod
+        Author rod = new Author("Rod","Johnson");
+        Book noEJB = new Book("J2EE Development without EJB","23444","Worx");
+        rod.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+}
