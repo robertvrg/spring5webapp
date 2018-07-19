c95073380d984c514dc3386b564c59ec0d57cf80
roba <varga.robert2@gmail.com>
2018-07-19 09:29:18 +0200

    first commit

---
+++ src/main/java/guru/springframework/sprint5webapp/Sprint5webappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sprint5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint5webappApplication.class, args);
+	}
+}
